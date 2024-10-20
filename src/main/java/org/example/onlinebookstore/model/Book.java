a5ba9e600f61e33e3d1cd4c9e015fe50cf4016da
Denys Mazurenko <0gur4ik3992@gmail.com>
2024-10-20 14:05:39 +0300

    removed cmd from gitatributes. Used annotations @getter @setter instead of @Data

--- src/main/java/org/example/onlinebookstore/model/Book.java
+++ src/main/java/org/example/onlinebookstore/model/Book.java
@@ -6,12 +6,15 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.math.BigDecimal;
-import lombok.Data;
 
 @Entity
 @Table(name = "books")
-@Data
+@Getter
+@Setter
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
