534a0e416f7ac17961f8721b99c50f4554838c0c
Denys Mazurenko <0gur4ik3992@gmail.com>
2024-10-21 00:31:44 +0300

    Implemented book, repository, service for book model (#1)
    * Implemented book, repository, service for book model
    
    * fix checkstyle error
    
    * User annotation requiredArgCountructor instead of autowired

---
+++ src/main/java/org/example/onlinebookstore/repository/BookRepositoryImpl.java
@@ -0,0 +1,51 @@
+package org.example.onlinebookstore.repository;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.example.onlinebookstore.exception.DataProcessingException;
+import org.example.onlinebookstore.model.Book;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+import org.springframework.stereotype.Repository;
+
+@RequiredArgsConstructor
+@Repository
+public class BookRepositoryImpl implements BookRepository {
+    private final SessionFactory sessionFactory;
+
+    @Override
+    public Book save(Book book) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(book);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't insert book "
+                    + book
+                    + " to db", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return book;
+    }
+
+    @Override
+    public List<Book> findAll() {
+        try (Session session = sessionFactory.openSession()) {
+            Query<Book> bookQuery = session.createQuery("from Book", Book.class);
+            return bookQuery.getResultList();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't find all books", e);
+        }
+    }
+}
